afe1eb0087b18f2a87c0a0a6b6bb75f2c0753875
ong-124 <ong__outlook.com>
2026-01-19 11:51:47 +0800

    fix 修改rust/lakesoul-metadata/src/lib.rs/clean_meta_for_test方法内容，去掉清表SQL

--- rust/lakesoul-metadata/src/lib.rs
+++ rust/lakesoul-metadata/src/lib.rs
@@ -1709,16 +1709,21 @@ pub async fn execute_query_scalar(
 }
 
 pub async fn clean_meta_for_test(client: &PooledClient) -> Result<i32> {
+    error!(
+        "有人调用了我！！！按照原油逻辑会清空元数据库，请仔细检查代码！！！\n数据无价！！！\n数据无价！！！\n数据无价！！！\n谨慎操作！！！\n谨慎操作！！！\n谨慎操作！！！: {:?}",
+        client
+    );
     let result = client
-        .batch_execute(
-            "delete from namespace;
-            delete from data_commit_info;
-            delete from table_info;
-            delete from table_path_id;
-            delete from table_name_id;
-            delete from partition_info;
-            delete from discard_compressed_file_info",
-        )
+        // .batch_execute(
+        //     "delete from namespace;
+        //     delete from data_commit_info;
+        //     delete from table_info;
+        //     delete from table_path_id;
+        //     delete from table_name_id;
+        //     delete from partition_info;
+        //     delete from discard_compressed_file_info",
+        // )
+        .batch_execute("SELECT 1;")
         .await;
     match result {
         Ok(_) => Ok(0i32),
